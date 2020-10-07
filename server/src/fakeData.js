6799d0dc277a3cf03e17cf8a82076ca2cb94a104
Reuben Smith <reuben-smith@hotmail.com>
2020-10-07 12:13:19 +1030

    Adds up-voting into the server API
    * Imports the articlesInfo fake data
    * Adds POST endpoint: /api/articles/:name/upvote
        - Increments the upvote by one for each POST request to :name
        - eg. :name => learn-react

---
+++ server/src/fakeData.js
@@ -0,0 +1,13 @@
+const articlesInfo = {
+    "learn-react": {
+        upvotes: 0,
+    },
+    "learn-node": {
+        upvotes: 0,
+    },
+    "my-thoughts-on-resumes": {
+        upvotes: 0,
+    },
+};
+
+export default articlesInfo;
--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import articlesInfo from "./fakeData";
 
 const PORT = 9000;
 const app = express();
@@ -8,16 +9,12 @@ const app = express();
  * adding body property to the req parameter to the matching route */
 app.use(bodyParser.json());
 
-app.get("/hello", (req, res) => {
-    res.send("Hello");
-});
-
-app.get("/hello/:name", (req, res) => {
-    res.send(`Hello ${req.params.name}`);
-});
-
-app.post("/hello", (req, res) => {
-    res.send(`Hello ${req.body.name}!`);
+app.post("/api/articles/:name/upvote", (req, res) => {
+    const articleName = req.params.name;
+    articlesInfo[articleName].upvotes += 1;
+    res.status(200)
+        .send(`${articleName} now has \
+        ${articlesInfo[articleName].upvotes} upvotes`);
 });
 
 const server = app.listen(PORT, () => {
