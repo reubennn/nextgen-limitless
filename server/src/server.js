0a4b72d8962abd889ca02904d11489c078981c27
Reuben Smith <reuben-smith@hotmail.com>
2020-10-07 11:20:08 +1030

    Adds POST endpoint and shuts down server on Node.js exit
    * Adds body-parser dependency
    * Adds POST endpoint for /hello
        - Uses body parser to get the request body
    * Adds process.on exit to close the server so PORT does not remain in use

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,28 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const PORT = 9000;
 const app = express();
+
+/* Parse JSON object included in the POST request,
+ * adding body property to the req parameter to the matching route */
+app.use(bodyParser.json());
 
 app.get("/hello", (req, res) => {
     res.send("Hello");
 });
 
-app.listen(PORT, () => {
+app.post("/hello", (req, res) => {
+    res.send(`Hello ${req.body.name}!`);
+});
+
+const server = app.listen(PORT, () => {
     console.log(`\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\
     \nServer running on PORT ${PORT}.\
     \n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n`);
+});
+
+/* Close the server before Node.js exits */
+process.on("exit", function () {
+    server.close();
 });
