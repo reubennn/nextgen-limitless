210d3686ed9c6221f71f732197d0bc3e80e66774
Reuben Smith <reuben-smith@hotmail.com>
2020-10-07 14:19:16 +1030

    Adds MongoDB to server to get article information
    * Adds dependency: "mongodb": "^3.6.2"
    * Adds GET endpoint: /api/articles/:name
        - Connects to localhost MongoDB
        - Fetches the article information using MongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { MongoClient } from "mongodb";
 import articlesInfo from "./fakeData";
 
 const PORT = 9000;
@@ -8,6 +9,26 @@ const app = express();
 /* Parse JSON object included in the POST request,
  * adding body property to the req parameter to the matching route */
 app.use(bodyParser.json());
+
+app.get("/api/articles/:name", async (req, res) => {
+    try {
+        const articleName = req.params.name;
+
+        const client = await MongoClient.connect("mongodb://localhost:27017", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        const db = client.db("my-blog");
+
+        const articleInfo = await db.collection("articles")
+            .findOne({ name: articleName });
+        res.status(200).json(articleInfo);
+
+        client.close();
+    } catch (error) {
+        res.stats(500).json({ message: "Error connecting to db", error });
+    }
+});
 
 app.post("/api/articles/:name/upvote", (req, res) => {
     const articleName = req.params.name;
