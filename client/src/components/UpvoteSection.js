8832f38007e4da4024eb606c3011a96ff4602639
Reuben Smith <reuben-smith@hotmail.com>
2021-02-23 15:55:52 +1030

    Adds protected routes to upvote/downvote an article
    *** components/UpvoteSection ***
    - Implements useSecuredApi Hook to call the respective protected endpoints
    from the API using bearer tokens from Auth0.
    - Ensures a user must be authenticated to upvote an article, otherwise
    they are redirected to the sign in page.
    - If a user has upvoted an article, the button changes, giving them an
    option to downvote it (perhaps if they change their mind).
    
    *** Server ***
    - Adds another endpoint and controller to downvote an article.
    - Protects the upvote/downvote article endpoints, ensuring only
    authenticated Auth0 users can access it.

--- client/src/components/UpvoteSection.js
+++ client/src/components/UpvoteSection.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import PropTypes from "prop-types";
+import { useAuth0 } from "@auth0/auth0-react";
+import useSecuredApi from "../hooks/useSecuredApi";
 
 import * as S from "../styles/styled-components/styled";
 
@@ -9,18 +11,110 @@ import * as S from "../styles/styled-components/styled";
  * @return {Component} upvote section
  */
 const UpvoteSection = ({ articlePath, upvotes, setArticle }) => {
+    const [hasUpvoted, setHasUpvoted] = useState(false);
+
+    /** useRef Mount status for cleanup and avoiding memory leaks  */
+    const isMounted = useRef(true);
+
+    /** Check if user is authenticated with Auth0 authentication */
+    const { isAuthenticated, loginWithRedirect } = useAuth0();
+
+    const upvoteRequestUrl = `/api/articles/${articlePath}/upvote`;
+    const downvoteRequestUrl = `/api/articles/${articlePath}/downvote`;
+    const requestOptions = { method: "POST" };
+
+    /** Custom React Hooks for sending HTTP request to secure API endpoints */
+    const {
+        state: upvoteRequestState,
+        securedApiRequest: securedApiUpvoteRequest,
+    } = useSecuredApi(upvoteRequestUrl, requestOptions);
+    const {
+        state: downvoteRequestState,
+        securedApiRequest: securedApiDownvoteRequest,
+    } = useSecuredApi(downvoteRequestUrl, requestOptions);
+
+    /**
+     * Handler function which redirects the user
+     * to the account log in page for Auth0, while storing the current
+     * url to the appState to use upon redirect callback.
+     */
+    const redirectToAuthLogin = () => {
+        loginWithRedirect({
+            /** Pass the current url so Auth0 knows where to redirect back to */
+            appState: {
+                returnTo: window.location.pathname,
+            },
+        });
+    };
+
     /**
      * Function called during upvote button on click event.
      * - Sends POST request to server API to add an upvote to the database.
      * - Updates the article info with the new upvote number.
      */
     const upvoteArticle = async () => {
-        const response = await fetch(`/api/articles/${articlePath}/upvote`, {
-            method: "POST",
-        });
-        const body = await response.json();
-        setArticle(body.value);
+        if (isAuthenticated) {
+            if (!hasUpvoted) {
+                securedApiUpvoteRequest();
+            } else {
+                securedApiDownvoteRequest();
+            }
+        } else {
+            redirectToAuthLogin();
+        }
     };
+
+    /**
+     * useEffect which performs cleanup, setting isMounted to false.
+     *
+     * - This ensures subscriptions and async tasks are cancelled
+     * while the component is unmounted.
+     */
+    useEffect(() => {
+        return () => {
+            isMounted.current = false;
+        };
+    }, []);
+
+    /**
+     * useEffect triggered when the upvoteRequestState receives a response
+     * from the API.
+     *
+     * - If no errors, update the article and set hasUpvoted flag.
+     */
+    useEffect(() => {
+        if (isMounted.current) {
+            if (upvoteRequestState.data !== null) {
+                const { body, response } = upvoteRequestState.data;
+                if (response.status === 200 && isMounted.current) {
+                    setArticle(body.value);
+                    setHasUpvoted(true);
+                }
+            } else if (upvoteRequestState.error !== null) {
+                console.error(upvoteRequestState.error);
+            }
+        }
+    }, [upvoteRequestState]);
+
+    /**
+     * useEffect triggered when the downvoteRequestState receives a response
+     * from the API.
+     *
+     * - If no errors, update the article and set hasUpvoted flag.
+     */
+    useEffect(() => {
+        if (isMounted.current) {
+            if (downvoteRequestState.data !== null) {
+                const { body, response } = downvoteRequestState.data;
+                if (response.status === 200 && isMounted.current) {
+                    setArticle(body.value);
+                    setHasUpvoted(false);
+                }
+            } else if (downvoteRequestState.error !== null) {
+                console.error(downvoteRequestState.error);
+            }
+        }
+    }, [downvoteRequestState]);
 
     /**
      * Check if upvotes are 1.
@@ -40,7 +134,7 @@ const UpvoteSection = ({ articlePath, upvotes, setArticle }) => {
                     className="upvote-button gradient justify-center"
                     $radius="0.5rem"
                     onClick={() => upvoteArticle()}>
-                    +1
+                    {hasUpvoted ? "-1" : "+1"}
                 </S.Button>
             </S.UpvoteSection>
             <S.HorizontalRuler thin smallMargin color="grey-tint-neutral" />
