639c56903bf2bdb40f49bb9a333466b6076c40a8
Reuben Smith <reuben-smith@hotmail.com>
2021-03-23 13:02:23 +1030

    Fixes 404 page not found robot image positioning
    - Adds scrollWithPage prop to ResponsiveImage component.

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -58,6 +58,7 @@ const NotFound = ({ item, viewport }) => {
                     <ResponsiveImage
                         srcset={pageNotFoundRobot}
                         background
+                        scrollWithPage
                         alt="Page not found broken robot holding lightbulb" />
                 </S.Section>
             </S.MainPageBody>
